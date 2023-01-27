cc010c1b2a498de7790f246a11b00c8bd61db4c4
elecinas <estherlecina@gmail.com>
2023-01-27 11:10:41 +0100

    Add props to Greeting.js component

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Greeting />
+        <Greeting name="Shaun" numberOfMessages={5}/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           This is {adjective}!
--- src/Greeting.js
+++ src/Greeting.js
@@ -1,14 +1,19 @@
 import React from "react";
 
-export const Greeting = () => {
+export const Greeting = ({ name, numberOfMessages }) => {
+  if (!name) return null;
   let isMorning = new Date().getHours() < 12;
-  let greetingHeader = isMorning
-    ?<h3>Good Morning!</h3>
-    :<h3>Good Evening!</h3>;
+  let greetingHeader = isMorning ? (
+    <h3>Good Morning {name}!</h3>
+  ) : (
+    <h3>Good Evening {name}!</h3>
+  );
   return (
     <>
       {greetingHeader}
-      <p>You have 5 messages</p>
+      {numberOfMessages === 0 ? null : (
+        <p>You have {numberOfMessages} messages</p>
+      )}
     </>
   );
-}
+};
