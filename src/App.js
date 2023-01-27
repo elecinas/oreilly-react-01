59f5b3a5706adb61a4675948cfff25808b29e6cd
elecinas <estherlecina@gmail.com>
2023-01-27 18:21:45 +0100

    useState in component CounterButton.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from "./logo.svg";
+import { CounterButton } from "./CounterButton";
 import "./App.css";
 import { Greeting } from "./Greeting";
 import { PeopleList } from "./PeopleList";
@@ -28,20 +29,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p style={{color:'red', fontSize: '96px'}}>Big Red Text!</p>
-        <Greeting name="Shaun" numberOfMessages={5} />
-        <PeopleList people={people} />
-        <button onClick={() => { alert("Hello!") }}>Click Me!</button>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>This is {adjective}!</p>
-        <a
-          className="App-link"
-          href={url}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <CounterButton />
       </header>
     </div>
   );
