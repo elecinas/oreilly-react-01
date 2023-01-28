2cf0a15084d17fabd50fb5cec667d5a76f603948
elecinas <estherlecina@gmail.com>
2023-01-28 20:21:08 +0100

    Refactor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from "./logo.svg";
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes, Link, Navigate } from "react-router-dom";
 import { HomePage, CounterButtonPage, PeopleListPage, NotFoundPage } from "./pages";
-import { CongratulationsMessage } from "./CongratulationsMessage";
-import { CounterButton } from "./CounterButton";
 import "./App.css";
-import { Greeting } from "./Greeting";
-import { PeopleList } from "./PeopleList";
 
 function App() {
-  let adjective = "cool";
-  let url = "https://reactjs.org";
-  const [numberOfClicks, setNumberOfClicks] = useState(0);
-  const [hideMessage, setHideMessage] = useState(false);
-  const increment = () => setNumberOfClicks(numberOfClicks + 1);
 
   return (
     <div className="App">
-
       <Router>
         <Link to="/counter">Go to Counter Page</Link>
         <Link to="/people-list">Go to People List Page</Link>
