a2a5fd5eea36dc7aaebb64b06e79c65996a0750c
elecinas <estherlecina@gmail.com>
2023-01-28 17:19:08 +0100

    Use useParams on CounterButtonPage.js

--- src/pages/CounterButtonPage.js
+++ src/pages/CounterButtonPage.js
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
+import { useParams } from "react-router-dom";
 import { CounterButton } from "../CounterButton";
 import { CongratulationsMessage } from "../CongratulationsMessage";
 
 export const CounterButtonPage = () => {
+  const { name } = useParams();
   const [numberOfClicks, setNumberOfClicks] = useState(0);
   const [hideMessage, setHideMessage] = useState(false);
   const increment = () => setNumberOfClicks(numberOfClicks + 1);
 
   return (
     <>
+      <h1>{name}'s Counter Button Page</h1>
       {hideMessage ? null : (
         <CongratulationsMessage
           numberOfClicks={numberOfClicks}
