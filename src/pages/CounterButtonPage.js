c4f87aa26dbe698b43a9edfa32ddf5a5857a4728
elecinas <estherlecina@gmail.com>
2023-07-18 09:32:31 +0200

    persistencia de datos en react

--- src/pages/CounterButtonPage.js
+++ src/pages/CounterButtonPage.js
@@ -1,16 +1,17 @@
-import React, { useState } from "react";
-import { useLocation } from "react-router-dom";
-import queryString from "query-string";
+import React, { useState, useEffect } from "react";
 import { CounterButton } from "../CounterButton";
 import { CongratulationsMessage } from "../CongratulationsMessage";
 import { DisplayIf } from "../DisplayIf";
 
 export const CounterButtonPage = () => {
 
-  const location = useLocation();
-  const startingValue = queryString.parse(location.search).startingValue || 0;
-  const [numberOfClicks, setNumberOfClicks] = useState(Number(startingValue));
+  const [numberOfClicks, setNumberOfClicks] = useState(
+    Number(localStorage.getItem('numberOfClicks')) || 0
+  );
   const [hideMessage, setHideMessage] = useState(false);
+  useEffect(() => {
+    localStorage.setItem('numberOfClicks', numberOfClicks);
+  }, [numberOfClicks])
   const increment = () => setNumberOfClicks(numberOfClicks + 1);
 
   return (
