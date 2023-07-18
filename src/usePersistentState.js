fa05fdc856883bf9fdd7c58114ae7be2d912884b
elecinas <estherlecina@gmail.com>
2023-07-18 09:59:44 +0200

    React Hooks personalizados: usePersistentState

---
+++ src/usePersistentState.js
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+
+export const usePersistentState = (storageKey, defaultValue, Type) => {
+  const [state, setState] = useState(
+    Number(localStorage.getItem(storageKey)) || defaultValue
+  );
+  useEffect(() => {
+    localStorage.setItem(storageKey, state);
+  }, [state]);
+
+  return [state, setState]
+};
